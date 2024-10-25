6e2bd4394c69c4bf4e1c24bbf5135fb278e3f1c3
INFINEON\britojen <jennifer.brito@infineon.com>
2024-10-25 15:39:56 +0100

    included time, icon and humidity in JS

--- src/index.js
+++ src/index.js
@@ -2,10 +2,35 @@ function updateWeather(response) {
     let tempElement = document.querySelector("#weather-current-temp");
     let temp = response.data.temperature.current;
     let cityElement = document.querySelector("#weather-city");
+    let descriptionElement = document.querySelector ("#weatherDescription");
+    let humidityElement = document.querySelector("#humidity");
+    let speedElement = document.querySelector("#speed");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
+    let icon = document.querySelector("#weather-icon");
+
+
     cityElement.innerHTML = response.data.city;
+    timeElement.innerHTML = formatDate(date);
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    speedElement.innerHTML = `${response.data.wind.speed}km/h`;
     tempElement.innerHTML = Math.round(temp);
+    icon.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-temp" />`;
 }
 
+function formatDate(date) {
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+
+    return `${day} ${hours}:${minutes}`;
+}
 
 function searchCity (city) {
     let apiKey = "65a24eoe6e271bf94a4a755eb31f805t";
