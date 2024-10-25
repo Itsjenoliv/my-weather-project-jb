f3209c71a3792db3a7829733e16efb3c9a6c7742
INFINEON\britojen <jennifer.brito@infineon.com>
2024-10-25 14:44:41 +0100

    included API for search

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
-function handleSubmit (event) {
+function updateWeather(response) {
+    let tempElement = document.querySelector("#weather-current-temp");
+    let temp = response.data.temperature.current;
+    let cityElement = document.querySelector("#weather-city");
+    cityElement.innerHTML = response.data.city;
+    tempElement.innerHTML = Math.round(temp);
+}
+
+
+function searchCity (city) {
+    let apiKey = "65a24eoe6e271bf94a4a755eb31f805t";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(updateWeather);
+}
+
+
+function handleSubmit (event) { 
     event.preventDefault();
     let searchInput = document.querySelector("#search-input");
-    let cityElement = document.querySelector("#weather-city");
-    cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
 }
 
 let searchElement = document.querySelector("#search");
 searchElement.addEventListener("submit", handleSubmit)
+
+searchCity("Lisbon");
